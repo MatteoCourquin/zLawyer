c47b0e07792d7cc833e3b4af922626e07676cacc
« Matteo <matteo.courquin@gmail.com>
2022-06-09 08:12:26 +0200

    :lipstick: Add Styles Components

--- components/Elements.vue
+++ components/Elements.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="py-10 border-t-8 border-black">
+
+        <!-- Typo -->
         <h1>h1 • Roboto-Black</h1>
         <h2>h2 • Roboto-Black</h2>
         <h3>h3 • Roboto-Medium</h3>
         <h4>h4 • Roboto-Regular</h4>
         <p>p • Roboto-Regular</p>
         <p>Texte <span class="text-underline">souligné</span></p>
-        <p>Texte <span class="text-bolder">souligné</span></p>
+        <p>Texte en <span class="text-bolder">gras</span></p>
         <p class="subtitle">Sous-titre • Roboto-Light</p>
+        <a href="#" class="text-link">Liens</a>
+        <li>
+            <ul>liste</ul>
+            <ul>liste</ul>
+            <ul>liste</ul>
+        </li>
+
+        <!-- Components -->
+        <button class="button-orange">Bouton orange</button>
+        <button class="button-transparent">Bouton transaprent</button>
+
+        <!-- Colors -->
         <div class="p-10 bg-white"><p class="text-black">white</p></div>
         <div class="p-10 bg-light-blue"><p class="text-black">light-blue</p></div>
         <div class="p-10 bg-blue"><p class="text-black">blue</p></div>
