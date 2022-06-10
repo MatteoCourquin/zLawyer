0d488054c154a569cd91f41fe79e06640e3a960b
« Matteo <matteo.courquin@gmail.com>
2022-06-10 11:02:10 +0200

    Hero Done :construction:

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,12 @@
 <template>
-    <div class="max-width-blue-section">
-        <h2>Hero</h2>
+    <div class="max-width-blue-section grid grid-cols-1 lg:grid-cols-2 gap-5 items-center">
+        <div class="flex flex-col items-center lg:block row-start-2 lg:row-start-1">
+            <h1 class="text-center lg:text-left">La solution de gestion de cabinets d’avocats <br><span class="text-orange">simple, <br>ergonomique, <br>sans engagement !</span></h1>
+            <button class="button-orange mt-14 w-max">Je souhaite une démonstration</button>
+        </div>
+        <div>
+            <img class="w-full" src="~/assets/images/img/illustration-hero.png" alt="illustration du logiciel">
+            <h4 class="text-center hidden lg:block">La solution la mieux notée par vos confrères !</h4>
+        </div>
     </div>
 </template>
