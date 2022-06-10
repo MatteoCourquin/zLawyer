16b14ace524747c1baca4e30697b25292b693415
« Matteo <matteo.courquin@gmail.com>
2022-06-10 11:24:27 +0200

    Review Google :Construction:

--- components/Reviews.vue
+++ components/Reviews.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="max-width-blue-section">
-        <h2>Avis Google</h2>
+        <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+        <div class="elfsight-app-c590a455-5d86-4c86-a2fc-0c0d7b74e8ac"></div>
     </div>
 </template>
