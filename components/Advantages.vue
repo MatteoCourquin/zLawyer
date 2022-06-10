b1489de569196f63729e5c6712a6e7a338178eb1
« Matteo <matteo.courquin@gmail.com>
2022-06-10 14:20:59 +0200

    Advantages Done :construction:

--- components/Advantages.vue
+++ components/Advantages.vue
@@ -1,5 +1,97 @@
 <template>
     <div class="max-width-blue-section">
         <h2>Les Avantages</h2>
+        <p class="subtitle md:w-2/3 xl:w-1/2">La puissance de zLawyer réside dans son parfait équilibre entre <span class="text-bolder">simplicité d’utilisation, richesse fonctionnelle et la qualité approuvée du service support.</span></p>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <!-- Card Advantages Exhaustivité -->
+            <div class="card-advantages">
+                <div class="card-advantages-head">
+                    <h3>Exhaustivité</h3>
+                    <svg class="logo-advantages" id="Groupe_2" data-name="Groupe 2" xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 54 54">
+                        <circle id="Ellipse_1" data-name="Ellipse 1" cx="27" cy="27" r="27" fill="#ed630d"/>
+                        <path id="Icon_ionic-ios-star" data-name="Icon ionic-ios-star" d="M29.179,12.372H19.988L17.2,4.037a1.012,1.012,0,0,0-1.9,0L12.5,12.372H3.25a1,1,0,0,0-1,1,.735.735,0,0,0,.019.169.96.96,0,0,0,.419.706l7.554,5.323L7.342,28a1,1,0,0,0,.344,1.125.967.967,0,0,0,.562.244,1.225,1.225,0,0,0,.625-.225l7.373-5.255,7.373,5.255a1.171,1.171,0,0,0,.625.225.9.9,0,0,0,.556-.244A.99.99,0,0,0,25.143,28l-2.9-8.429L29.735,14.2l.181-.156a1.048,1.048,0,0,0,.325-.669A1.058,1.058,0,0,0,29.179,12.372Z" transform="translate(10.755 10.629)" fill="#fff"/>
+                    </svg>
+                </div>
+                <p>Année après année, nos partenaires avocats ont fait évoluer le logiciel avocat zLawyer pour le rendre tout simplement <span class="text-bolder">complet</span> afin de répondre à l’ensemble de vos besoins : la <span class="text-bolder">gestion de vos dossiers</span>, de <span class="text-bolder">vos temps</span>, la <span class="text-bolder">facturation des honoraires</span>, les <span class="text-bolder">frais et des débours</span>, le <span class="text-bolder">suivi</span>, <span class="text-bolder">relance automatique des impayés</span>, la <span class="text-bolder">gestion documentaire</span>, l’<span class="text-bolder">agenda</span>, les <span class="text-bolder">tâches</span>, <span class="text-bolder">export comptable</span>, <span class="text-bolder">tableau de bord</span>, etc …
+                </p>
+            </div>
+
+            <!-- Card Advantages Service -->
+            <div class="card-advantages">
+                <div class="card-advantages-head">
+                    <h3>Service</h3>
+                    <svg class="logo-advantages" xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 54 54">
+                        <g id="Groupe_2" data-name="Groupe 2" transform="translate(-0.379)">
+                            <circle id="Ellipse_1" data-name="Ellipse 1" cx="27" cy="27" r="27" transform="translate(0.379)" fill="#ed630d"/>
+                            <path id="Icon_awesome-handshake" data-name="Icon awesome-handshake" d="M21.583,4.5H17.318a1.5,1.5,0,0,0-1.072.468l-4.881,5.02c0,.006-.01.017-.015.022a2.416,2.416,0,0,0-.1,3.123,1.908,1.908,0,0,0,2.785.151c0-.006.015-.006.02-.011L18.018,9.19a.738.738,0,0,1,1.122.056.97.97,0,0,1-.05,1.26l-1.3,1.333,7.229,6.592a3.813,3.813,0,0,1,.392.429V8.069L22.705,5.024A1.5,1.5,0,0,0,21.583,4.5ZM27.01,8.081V20.568A1.694,1.694,0,0,0,28.6,22.353h3.178V8.081Zm2.383,12.488a.9.9,0,1,1,.794-.892A.85.85,0,0,1,29.394,20.568ZM0,22.347H3.178a1.694,1.694,0,0,0,1.589-1.785V8.081H0Zm2.383-3.564a.9.9,0,0,1,0,1.785.9.9,0,0,1,0-1.785Zm21.643,1.037-7.413-6.76-1.49,1.534a3.311,3.311,0,0,1-5.05-.245,4.374,4.374,0,0,1,.218-5.672L14.354,4.5H10.193a1.5,1.5,0,0,0-1.122.524L6.355,8.069V20.557h.909l4.493,4.568a2.946,2.946,0,0,0,4.469-.519l.01-.011.889.864a1.711,1.711,0,0,0,2.6-.3l1.559-2.153.268.245a1.471,1.471,0,0,0,2.234-.262l.472-.653a1.942,1.942,0,0,0-.228-2.515Z" transform="translate(11.112 11.788)" fill="#fff"/>
+                        </g>
+                    </svg>
+                </div>
+                <p>De la formation à l’outil en passant par la hotline, un seul mot d’ordre: chaque utilisateur a chez nous le statut <span class="text-bolder">Premium</span>. VOTRE besoin est notre ADN. Voilà pourquoi <span class="text-bolder">disponibilité</span>, <span class="text-bolder">rapidité</span>, <span class="text-bolder">amabilité</span> et <span class="text-bolder">efficacité</span> sont devenues les maîtres-mots de notre logiciel .
+                </p>
+            </div>
+
+            <!-- Card Advantages Facilité -->
+            <div class="card-advantages">
+                <div class="card-advantages-head">
+                    <h3>Facilité</h3>
+                    <svg class="logo-advantages" id="Groupe_2" data-name="Groupe 2" xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 54 54">
+                        <circle id="Ellipse_1" data-name="Ellipse 1" cx="27" cy="27" r="27" fill="#ed630d"/>
+                        <path id="Icon_material-thumb-up" data-name="Icon material-thumb-up" d="M1.5,24.288H6.058V10.615H1.5ZM26.567,11.755a2.286,2.286,0,0,0-2.279-2.279H17.1l1.082-5.207.034-.365a1.715,1.715,0,0,0-.5-1.208L16.506,1.5l-7.5,7.509a2.228,2.228,0,0,0-.672,1.607V22.009a2.286,2.286,0,0,0,2.279,2.279H20.87a2.263,2.263,0,0,0,2.1-1.39l3.441-8.033a2.251,2.251,0,0,0,.16-.832V11.857l-.011-.011Z" transform="translate(12.967 14.106)" fill="#fff"/>
+                    </svg>
+                </div>
+                <p>Sous la tutelle des utilisateurs eux-mêmes et afin de leur faire <span class="text-bolder">gagner du temps</span>, TOUT a été pensé dans ce logiciel pour avocat zLawyer pour être <span class="text-bolder">ultra simple d’utilisation</span> et <span class="text-bolder">intuitif</span>. De la saisie des temps en passant par la gestion des dossiers et évidemment l’émission de factures de la plus simple à la plus complexe, on comprend que ce sont les avocats eux-mêmes qui ont pensé l’outil pour leurs cabinets.
+                </p>
+                <br>
+                <p>
+                L’outil est <span class="text-bolder">personnalisable</span> afin de répondre à votre propre besoin et aller droit au but <span class="text-bolder">rapidement</span> et <span class="text-bolder">facilement</span>. 
+                </p>
+            </div>
+
+            <!-- Card Advantages Évolutif -->
+            <div class="card-advantages">
+                <div class="card-advantages-head">
+                    <h3>Évolutif</h3>
+                    <svg class="logo-advantages" id="Groupe_2" data-name="Groupe 2" xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 54 54">
+                        <circle id="Ellipse_1" data-name="Ellipse 1" cx="27" cy="27" r="27" fill="#ed630d"/>
+                        <path id="Icon_awesome-balance-scale" data-name="Icon awesome-balance-scale" d="M11.466,15.049h0c0-.725.06-.391-3.809-8.13a2.152,2.152,0,0,0-3.846,0C-.092,14.725,0,14.348,0,15.049H0c0,1.979,2.567,3.583,5.733,3.583S11.466,17.028,11.466,15.049ZM5.733,7.883l3.225,6.45H2.508Zm22.932,7.166c0-.725.06-.391-3.809-8.13a2.152,2.152,0,0,0-3.846,0c-3.9,7.805-3.809,7.428-3.809,8.13h0c0,1.979,2.567,3.583,5.733,3.583s5.733-1.6,5.733-3.583Zm-8.957-.717,3.225-6.45,3.225,6.45Zm3.942,5.733H15.766V6.864A3.586,3.586,0,0,0,17.844,4.3h5.805a.716.716,0,0,0,.717-.717V2.15a.716.716,0,0,0-.717-.717H17.183a3.551,3.551,0,0,0-5.7,0H5.016A.716.716,0,0,0,4.3,2.15V3.583a.716.716,0,0,0,.717.717h5.805A3.586,3.586,0,0,0,12.9,6.864v13.2H5.016a.716.716,0,0,0-.717.717v1.433a.716.716,0,0,0,.717.717H23.649a.716.716,0,0,0,.717-.717V20.783A.716.716,0,0,0,23.649,20.066Z" transform="translate(12.667 15.534)" fill="#fff"/>
+                    </svg>
+                </div>
+                <p>zLawyer est la simple <span class="text-bolder">traduction des besoins exprimés par les avocats</span> eux-mêmes. Conçu dès 2006 avec un pool d’avocats testeurs, zLawyer n’a été commercialisé qu’après 2 ans et ½ de <span class="text-bolder">Recherche et Développement</span> visant à vous fournir un <span class="text-bolder">software naturellement utile et en constante évolution</span>. Vous bénéficiez de nouvelles fonctionnalités régulièrement et sans impactes budgétaires sur votre abonnement. 
+                </p>
+            </div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+    .card-advantages {
+        box-shadow: 0px 0px 20px var(--color-blue);
+        background-color: var(--color-white);
+        border-radius: var(--radius-main);
+        padding: 80px 50px;
+    }
+    .card-advantages h3{
+        color: var(--color-orange);
+        text-transform: uppercase;
+    }
+    .card-advantages .card-advantages-head{
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 50px;
+    }
+    @media screen and (max-width : 425px) {
+        .card-advantages{
+            padding: 40px;
+        }
+        .card-advantages .card-advantages-head{
+            flex-direction: column-reverse;
+        }
+        .logo-advantages{
+            margin-bottom: 30px;
+            align-self: flex-end;
+        }
+    }
+
+</style>
