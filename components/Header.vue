f944ef30205944a50b7f1f5ef87a7c7482c21bbb
« Matteo <matteo.courquin@gmail.com>
2022-06-10 09:48:52 +0200

    Header Done :construction:

--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="max-w-max-width-screen mx-auto h-32 flex items-center justify-between px-8 py-6">
         <img src="~/assets/images/logos/logo-zele.png" class="h-full"/>
-        <li class="flex items-center">
-            <ul class="ml-10"><a class="text-link" href="#">Solutions</a></ul>
-            <ul class="ml-10"><a class="text-link" href="#">Fonctionnalités</a></ul>
-            <ul class="ml-10"><a class="text-link" href="#">Nos Clients</a></ul>
-            <ul class="ml-10"><a class="text-link" href="#">Web App</a></ul>
-            <ul class="ml-10"><a class="text-link" href="#"><button class="button-orange">Contact</button></a></ul>
-        </li>
-        <p>{{ isActive }}</p> 
-        <div class="burger">
-            <div class="btn_burger btn1" @click="isActive = 'active'">
+        <ul class="items-center hidden xl:flex header-nav" :class="{ active: isActive }">
+            <li class="ml-10"><a class="text-link" href="#">Solutions</a></li>
+            <li class="ml-10"><a class="text-link" href="#">Fonctionnalités</a></li>
+            <li class="ml-10"><a class="text-link" href="#">Nos Clients</a></li>
+            <li class="ml-10"><a class="text-link" href="#">Web App</a></li>
+            <li class="ml-10"><a class="text-link" href="#"><button class="button-orange">Contact</button></a></li>
+        </ul>
+        <div class="burger block xl:hidden">
+            <div class="btn_burger btn1" :class="{ active: isActive }" @click="isActive = !isActive">
                 <div class="lignes"></div>
                 <div class="lignes"></div>
                 <div class="lignes"></div>
@@ -23,7 +22,7 @@
     export default {
         data() {
             return {
-                isActive: 'hello',
+                isActive: false,
             }
         },
     }
@@ -34,9 +33,10 @@
         width: 60px;
         height: 70px;
         position: relative;
+        cursor: pointer;
     }
     .lignes{
-        background-color: var(--color-dark-blue);
+        background-color: var(--color-orange);
         width: 100%;
         height: 5px;
         border-radius: 5px;
@@ -52,7 +52,9 @@
     .lignes:nth-child(3){
         top: 36px;
     }
-
+    .active.btn1 .lignes{
+        background-color: var(--color-white);
+    }
     .active.btn1 .lignes:nth-child(1){
         top: 18px;
         transform: rotate(135deg);
@@ -64,5 +66,43 @@
     .active.btn1 .lignes:nth-child(3){
         top: 18px;
         transform: rotate(-135deg);
+    }
+
+    .active.header-nav{
+        position: absolute;
+        right: 0;
+        width: 50%;
+        height: 100%;
+        background: #0a0a0ab4;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        animation: toTopBurger .5s ease-in-out forwards;
+    }
+    @media screen and (max-width : 700px){
+        .active.header-nav{
+            width: 100%;
+        }
+    }
+    .active.header-nav li{
+        margin-top: 50px;
+        margin-left: 0;
+        transform: translateY(-80px);
+    }
+    .active.header-nav li a{
+        color: var(--color-white);
+    }
+    .active.header-nav li a:hover{
+        color: var(--color-orange);
+    }
+
+    @keyframes toTopBurger {
+        from{
+            top: -100%;
+        }
+        to{
+            top: 0;
+        }
     }
 </style>
