fa5e356ba2ffc30bcb6376609fccc4b097c3d02d
Augustin <augustin.briolon@gmail.com>
2022-06-13 21:29:52 +0200

    :lipstick: space between wrap image

--- components/index/Clients.vue
+++ components/index/Clients.vue
@@ -9,6 +9,14 @@
           <img :src="archers" alt="Archers Logo" class="image w-auto" />
           <img :src="lexcom" alt="Lexcom Logo" class="image w-auto" />
           <img :src="rivedroit" alt="Rivedroit Logo" class="image w-auto" />
+          <img :src="rivedroit" alt="Rivedroit Logo" class="image w-auto" />
+        </div>
+        <div class="imagesContainer w-full flex items-center justify-between">
+          <img :src="aramsis" alt="Arasmis Logo" class="image w-auto" />
+          <img :src="archers" alt="Archers Logo" class="image w-auto" />
+          <img :src="archers" alt="Archers Logo" class="image w-auto" />
+          <img :src="lexcom" alt="Lexcom Logo" class="image w-auto" />
+          <img :src="rivedroit" alt="Rivedroit Logo" class="image w-auto" />
         </div>
       </div>
 
@@ -17,7 +25,21 @@
           class="imagesContainerBottom w-full flex items-center justify-between"
         >
           <img :src="aramsis" alt="Arasmis Logo" class="imageBottom w-auto" />
+          <img :src="aramsis" alt="Arasmis Logo" class="imageBottom w-auto" />
           <img :src="archers" alt="Archers Logo" class="imageBottom w-auto" />
+          <img :src="lexcom" alt="Lexcom Logo" class="imageBottom w-auto" />
+          <img
+            :src="rivedroit"
+            alt="Rivedroit Logo"
+            class="imageBottom w-auto"
+          />
+        </div>
+        <div
+          class="imagesContainerBottom w-full flex items-center justify-between"
+        >
+          <img :src="aramsis" alt="Arasmis Logo" class="imageBottom w-auto" />
+          <img :src="archers" alt="Archers Logo" class="imageBottom w-auto" />
+          <img :src="lexcom" alt="Lexcom Logo" class="imageBottom w-auto" />
           <img :src="lexcom" alt="Lexcom Logo" class="imageBottom w-auto" />
           <img
             :src="rivedroit"
@@ -31,10 +53,10 @@
 </template>
 
 <script>
-import aramsis from "../assets/images/clients/aramisLogo.png";
-import archers from "../assets/images/clients/archersLogo.png";
-import lexcom from "../assets/images/clients/lexcomLogo.png";
-import rivedroit from "../assets/images/clients/rivedroitLogo.png";
+import aramsis from "../../assets/images/clients/aramisLogo.png";
+import archers from "../../assets/images/clients/archersLogo.png";
+import lexcom from "../../assets/images/clients/lexcomLogo.png";
+import rivedroit from "../../assets/images/clients/rivedroitLogo.png";
 import { gsap } from "gsap";
 
 export default {
@@ -48,50 +70,68 @@ export default {
   },
 
   mounted() {
-    this.wrapAnimationInfiniteBottom();
-    this.wrapAnimationInfinite();
+    this.wrapAnimationInfinite()
+    this.wrapAnimationInfiniteBottom()
   },
 
   methods: {
     wrapAnimationInfinite() {
-      gsap.set(".wrapperContainerTop", { xPercent: -50 });
 
-      let imagesWidth = 300;
-      var totalWidth = imagesWidth * 10;
-      var test2 = document.querySelectorAll(".wrapperContainerTop .image");
-      var dirFromRight = "-=" + totalWidth;
+      if(window.matchMedia("(max-width: 1200px)").matches) {
 
-      var mod = gsap.utils.wrap(0, totalWidth);
+        gsap.set(".wrapperContainerBottom", { xPercent: -200 })
+
+      } else {
+
+        gsap.set(".wrapperContainerTop", { xPercent: -50 })
+
+      }
+
+      let imagesWidth = 300
+      var totalWidth = imagesWidth * 10
+      var test2 = document.querySelectorAll(".wrapperContainerTop .image")
+      var dirFromRight = "-=" + totalWidth
+
+      var mod = gsap.utils.wrap(0, totalWidth)
 
       gsap.set(test2, {
         x: function (i) {
-          return i * imagesWidth;
+          return i * imagesWidth
         },
-      });
+      })
 
       var action = gsap.timeline().to(test2, {
         x: dirFromRight,
         modifiers: {
           x: (x) => mod(parseFloat(x)) + "px",
         },
-        duration: 5,
+        duration: 25,
         ease: "none",
         repeat: -1,
-      });
+      })
     },
 
     wrapAnimationInfiniteBottom() {
-      gsap.set(".wrapperContainerBottom", { xPercent: -50 });
+
+      if(window.matchMedia("(max-width: 1200px)").matches) {
+
+        gsap.set(".wrapperContainerBottom", { xPercent: -200 })
+
+      } else {
+
+        gsap.set(".wrapperContainerBottom", { xPercent: -50 })
+
+      }
 
       // const imagesContainer = document.querySelector(".imagesContainerBottom")
-      const images = document.querySelectorAll(".imageBottom");
+      const images = document.querySelectorAll(".imageBottom")
 
-      let imagesWidth = 300;
-      var totalWidth = imagesWidth * 10;
+      let imagesWidth = 350
+      var totalWidth = imagesWidth * 10
       var test2 = document.querySelectorAll(
         ".wrapperContainerBottom .imageBottom"
       );
-      var dirFromLeft = "+=" + totalWidth;
+      var dirFromLeft = "+=" + totalWidth
 
       var mod = gsap.utils.wrap(0, totalWidth);
 
@@ -106,13 +146,18 @@ export default {
         modifiers: {
           x: (x) => mod(parseFloat(x)) + "px",
         },
-        duration: 5,
+        duration: 25,
         ease: "none",
         repeat: -1,
-      });
+      })
+    },
+
+    resizeFunction() {
+      this.wrapAnimationInfinite()
+      this.wrapAnimationInfiniteBottom()
     },
   },
-};
+}
 </script>
 
 <style scoped>
@@ -120,5 +165,9 @@ export default {
 .image {
   position: absolute;
   margin: 0 300px;
+}
+
+.wrapperContainerBottom {
+  padding-top: 100px;
 }
 </style>
